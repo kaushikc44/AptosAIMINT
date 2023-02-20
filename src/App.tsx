1451dd8c90fb3dcd15ae4c02b55d02955f1a525a
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-02-20 16:56:45 +0530

    formated lading page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React, {useState,Dispatch, SetStateAction} from 'react';
+import React, { useState, Dispatch, SetStateAction } from 'react';
 import './App.css';
 import WalletSpecifier from './components/wallet';
 import Navbar from './components/navbar';
 import ImageDisplay from './components/imagedisplay';
 import Textarea from './components/textae';
 import Aptos from './components/aptosnftcreator';
-
+import Form from './components/form';
 
 
 
 function App() {
-  const [discounnect,setdiscounnect] = useState<boolean>(false);
-  const [text,setText] = useState<string>("Enter prompt here!");
-  const [address,setAddress] = useState<string>("0x2845d80774ccf3ae0b02e39a149aff6a4f7b5a69fa59e2aa33d1d195d92693c3");
-  
+  const [discounnect, setdiscounnect] = useState<boolean>(false);
+  const [text, setText] = useState<string>("Enter prompt here!");
+  const [address, setAddress] = useState<string>("0x2845d80774ccf3ae0b02e39a149aff6a4f7b5a69fa59e2aa33d1d195d92693c3");
+
   return (
     <>
-  
+
       {/* <WalletSpecifier setAddress={setAddress} setdiscounnect={setdiscounnect} text={text}/> */}
       <Navbar address={address} />
-      <ImageDisplay />
-      <Textarea setText={setText} text={text}/>
+      <div className='flex justify-center items-center'>
+        <Form />
+        <ImageDisplay />
+      </div>
+      {/* <Textarea setText={setText} text={text}/> */}
       {/* <Aptos /> */}
 
     </>
